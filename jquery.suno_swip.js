d801785fd572aea4d36d49010a3a5e1cd7a32ca8
joyoko <18314@qq.com>
2014-10-17 09:09:39 +0800

    optimize some code

--- jquery.suno_swip.js
+++ jquery.suno_swip.js
@@ -4,67 +4,61 @@
 (function($){
     $.fn.extend({
         config:{},
-        swipConfig:{
-            canTouch:false,
-            startX:0,
-            startY:0,
-            transX:0,
-            transY:0
-        },
-        addHandler : function(elem,evtype,fn){
-            if(elem.attachEvent){
-                elem.attachEvent('on'+evtype,fn);
-            }else if(elem.addEventListener){
-                elem.addEventListener(evtype,fn,false);
+        startX:0,
+        startY:0,
+        transX:0,
+        transY:0,
+        addHandler : function(obj,type,fn){
+            if(obj.attachEvent){
+                obj.attachEvent('on'+type,fn);
+            }else if(obj.addEventListener){
+                obj.addEventListener(type,fn,false);
             }else{
-                elem["on"+evtype] = fn;
+                obj["on"+type] = fn;
             }
         },
-        bind_fn : function(obj,func){
+        bind_fn : function(obj,fn){
             return function(){
-                func.apply(obj,arguments);
+                fn.apply(obj,arguments);
             }
         },
         swip: function (config) {
             this.config = config;
-            if(!this.swipConfig.canTouch){
-                this.swipConfig.canTouch = true;
-                this.addHandler($(this)[0],"touchstart",this.bind_fn(this,this.touch_start));
-                this.addHandler($(this)[0],"touchmove",this.bind_fn(this,this.touch_move));
-                this.addHandler($(this)[0],"touchend",this.bind_fn(this,this.touch_end));
-            }
+            this.addHandler($(this)[0],"touchstart",this.bind_fn(this,this.touch_start));
+            this.addHandler($(this)[0],"touchmove",this.bind_fn(this,this.touch_move));
+            this.addHandler($(this)[0],"touchend",this.bind_fn(this,this.touch_end));
             return $(this);
         },
         touch_start : function(e){
             if(!event.touches.length) return;
             var touch = event.touches[0];
-            this.swipConfig.startX = touch.pageX;
-            this.swipConfig.startY = touch.pageY;
-            this.swipConfig.transX = 0;
-            this.swipConfig.transY = 0;
+            this.startX = touch.pageX;
+            this.startY = touch.pageY;
+            this.transX = 0;
+            this.transY = 0;
         },
         touch_move : function(e){
             if(!event.touches.length) return;
             var touch = event.touches[0];
-            this.swipConfig.transX = this.swipConfig.startX-touch.pageX;
-            this.swipConfig.transY = this.swipConfig.startY-touch.pageY;
-            if(this.config.moving != null)this.config.moving(this.swipConfig.transX,this.swipConfig.transY);
+            this.transX = this.startX-touch.pageX;
+            this.transY = this.startY-touch.pageY;
+            if(this.config.moving != null)this.config.moving(this.transX,this.transY);
             e.preventDefault();
         },
         touch_end : function(){
             //此处做swip分流
-            var absX = Math.abs(this.swipConfig.transX);
-            var absY = Math.abs(this.swipConfig.transY);
+            var absX = Math.abs(this.transX);
+            var absY = Math.abs(this.transY);
             if(absX > 20 && absX > absY){
                 //分流到left和right方向
-                if(this.swipConfig.transX < 0){
+                if(this.transX < 0){
                     if(this.config.right != null)this.config.right.apply(this,arguments);
                 }else{
                     if(this.config.left != null)this.config.left.apply(this,arguments);
                 }
             }else if(absY > 20 && absY > absX){
                 //分流到top和bottom方向
-                if(this.swipConfig.transY < 0){
+                if(this.transY < 0){
                     if(this.config.bottom != null)this.config.bottom.apply(this,arguments);
                 }else{
                     if(this.config.top != null)this.config.top.apply(this,arguments);
