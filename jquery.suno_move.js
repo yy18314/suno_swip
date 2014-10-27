7a7075d7b3d6b37449327c7561372072201c23bb
joyoko <18314@qq.com>
2014-10-27 11:26:35 +0800

    update some file

--- jquery.suno_move.js
+++ jquery.suno_move.js
@@ -7,34 +7,78 @@
         wrap:null,
         move:function(type,config,callback){
             this.callback = callback;
+            this.wrap = config.wrap || window;
+            var cx = config.center? $(this.wrap).width() / 2:0;
             switch(type){
                 case "left":
-                    $(this).animate({top:config.y,left:-$(this).width()},0);
+                    $(this).animate({top:config.y,left:-$(this).width(),opacity:0},0);
                     break;
                 case "right":
-                    this.wrap = config.wrap || window;
-                    $(this).animate({top:config.y,left:$(this.wrap).width()},0);
+                    $(this).animate({top:config.y,left:$(this.wrap).width(),opacity:0},0);
                     break;
                 case "bottom":
-                    this.wrap = config.wrap || window;
-                    $(this).animate({top:$(this.wrap).height(),left:config.x},0);
+                    $(this).animate({top:$(this.wrap).height(),left:config.x + cx,opacity:0},0);
                     break;
                 case "top":
-                    $(this).animate({top:-$(this).height(),left:config.x},0);
+                    $(this).animate({top:-$(this).height(),left:config.x + cx,opacity:0},0);
                     break;
-
                 default:moveTo(config);break;
             }
             this.moveTo(config,this.callback);
             return $(this);
         },
+        zoomTo:function(config,callback){
+            //缩放到指定位置
+            this.callback = callback;
+            this.wrap = config.wrap || window;
+            var x = config.x;
+            if(config.center){
+                //相对中线的坐标（做x方向的坐标变换）
+                x = config.x + $(this.wrap).width() / 2;
+            }
+            var y = config.y;
+            var time = config.time || 300;
+            var opacity = config.opacity || 1;
+            $(this).animate({left:x,top:y,opacity:opacity},time,"swing",callback);
+
+        },
+        moveOut:function(type,config,callback){
+            //移出去
+            this.callback = callback;
+            this.wrap = config.wrap || window;
+            var cx = config.center? $(this.wrap).width() / 2:0;
+            switch(type){
+                case "left":
+                    config.x = -$(this.wrap).width();
+                    break;
+                case "right":
+                    config.x = $(this.wrap).width();
+                    break;
+                case "bottom":
+                    config.y = $(this.wrap).height();
+                    config.x = config.x + cx;
+                    break;
+                case "top":
+                    config.y = -$(this.wrap).height();
+                    config.x = config.x + cx;
+                    break;
+                default:break;
+            }
+            config.opacity = 0;
+            this.moveTo(config,this.callback);
+            return $(this);
+        },
         moveTo: function (config,callback) {
             var x = config.x;
+            if(config.center){
+                //相对中线的坐标（做x方向的坐标变换）
+                x = config.x + $(this.wrap).width() / 2;
+            }
             var y = config.y;
             var time = config.time || 300;
             var opacity = config.opacity || 1;
             callback = callback || this.callback;
-            $(this).animate({left:x,top:y,opacity:opacity},time,callback);
+            $(this).animate({left:x,top:y,opacity:opacity},time,"swing",callback);
         },
         steps:function(configs,index){
             var config = configs[index];
@@ -44,6 +88,28 @@
                     that.steps(configs,index + 1);
                 }
             })
+        },
+        init_move:function(doms,steps){
+            if(doms.length == steps.length){
+                for(var i = 0 ; i < doms.length ; i ++){
+                    var dom = doms[i];
+                    var step = steps[i];
+                    var _type = step[0];
+                    var _config = {x:step[1],y:step[2],center:step[3],time:step[4]};
+                    $(dom).move(_type,_config);
+                }
+            }
+        },init_move_out:function(doms,steps){
+            //把所有元素移除到屏幕外
+            if(doms.length == steps.length){
+                for(var i = 0 ; i < doms.length ; i ++){
+                    var dom = doms[i];
+                    var step = steps[i];
+                    var _type = step[0];
+                    var _config = {x:step[1],y:step[2],center:step[3],time:step[4]};
+                    $(dom).moveOut(_type,_config);
+                }
+            }
         }
     });
 })(jQuery);
--- jquery.suno_pager.js
+++ jquery.suno_pager.js
@@ -3,28 +3,61 @@
  */
 (function($){
     $.fn.extend({
+        winH:document.documentElement.clientHeight,
+        dx:0,
+        dy:0,
+        index:0,
+        prev:null,
+        next:null,
         pages:new Array(),
-        init:function(type){
-            this.pages = $(this).find(".suno_page"); //初始化页面
-            $(this).css("position","relative");
-            if(this.pages.length > 1){
-                for(var i = 0 ; i < this.pages.length ; i ++) {
-                    var page = this.pages[i];
-                    $(page).swip({
-                        top:function(){
-                            //上翻一页
-                        },
-                        bottom:function(){
-                            //下翻一页
-                        },
-                        moving:function(x,y){
-                            //移动中
-                        }
-                    });
+        init_page:function(type){
+            this.pages = $(this).find(".suno-page");
+            for(var i = 0 ; i < this.pages.length ; i ++) {
+                var page = this.pages[i];
+                if(i == 0){
+                    page.style.top = 0 + "px";
+                }else{
+                    page.style.top = -1000 + "px";
                 }
-            }else{
-                //只有不足一页，别翻页了
             }
+            var that = this;
+            $(this).swip({
+                bottom:function(){
+                    //下翻操作
+                    if(that.index > 0){
+                        that.index -= 1;
+                        $(that.prev).animate({top:that.winH},300);
+                        $(that.next).animate({top:0},300);
+                    }
+                },top:function(){
+                    //上翻操作
+                    if(that.index < that.pages.length - 1){
+                        that.index += 1;
+                        $(that.prev).animate({top:-that.winH},300);
+                        $(that.next).animate({top:0},300);
+                    }
+                },moving:function(x,y){
+                    that.dx = x;
+                    that.dy = y;
+                    that.prev = that.pages[that.index];
+                    that.prev.style.zIndex = 50;
+                    if(that.dy > 0){
+                        if(that.index + 1 < that.pages.length){
+                            that.next = that.pages[that.index +1];
+                            that.next.zIndex = 20;
+                            $(that.prev).animate({top:-that.dy},0);
+                            $(that.next).animate({top:that.winH - that.dy},0);
+                        }
+                    }else{
+                        if(that.index - 1 >= 0) {
+                            that.next = that.pages[that.index -1];
+                            that.next.zIndex = 20;
+                            $(that.prev).animate({top:-that.dy},0);
+                            $(that.next).animate({top:-that.winH - that.dy},0);
+                        }
+                    }
+                }
+            });
         }
     });
 })(jQuery);
